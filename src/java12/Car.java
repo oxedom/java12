0d5a29aa1bd0ebc50d87c737fccf912c4f25080e
SamLinux <oxedom@gmail.com>
2022-11-30 23:53:11 +0200

    added comments

--- src/java12/Car.java
+++ src/java12/Car.java
@@ -8,7 +8,12 @@ public class Car {
     private String _brand;
     private boolean _isManual;
 
-
+    //Sam notes; Make sure that a user can't enter a int under 0 or overthose max numbers;
+    //type of car rank must be A B C D If the type isn't one of those set default to A
+    //MAKE SURE THAT BRAND ISNT NULL WHEN OBJECT IS CREATED 
+    final String DEFAULT_TYPE = "A";
+    final int MAX_ID = 9999999;
+    
 
     // constructor
     Car(int id, char type, String brand, boolean isManual ) {
@@ -19,7 +24,23 @@ public class Car {
      
     }
     
+    //Read about having two constructors with the same name, I think it's called overloading or something
+    //like that, maybe this method copies the car, check it out;
+    public Car(Car other) {}
+    
     // methods
+    //THINGS TO MAKE
+    //getId, getType, getBrand, isManual
+    //MAKE SURE THEY ARE FOOL PROOF NO BUGS TO BYPASS THEM;
+    //Setters, setId setType setBrand setIsManual;
+//    public String toString()
+    //toString SHOULD RETURNA SOMETHING LIKE
+//    id:1234567 type:A brand:Mazda gear:manual
+//    id:8877665 type:D brand:Toyota gear:auto
+//    id:9999999 type:B brand:Renault gear:manual
+    	
+    //OVERRIDE EQUALS METHOD TO CHECK IF TWO CARS HAVE THE SAME PROPS;
+    
     void increaseSpeed() {
         System.out.println("Cock");
     }

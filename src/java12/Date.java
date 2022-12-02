a2e97ca491e6be309f63538fbd5f6f12c3578144
=oxedom <oxedom@gmail.com>
2022-12-02 11:14:25 +0200

    init date obj

--- src/java12/Date.java
+++ src/java12/Date.java
@@ -1,5 +1,28 @@
 package java12;
 
 public class Date {
+	
+	private int _day;
+	private int _month;
+	private int _year;
+	
+	final int MAX_DAY = 31;
+	final int MAX_MONTH = 12;
+	final int MAX_YEAR = 9999;
+	
+	//CHECK IF LEGAL MAKE LEGAL FUNCTIONS;
+	//CHECK FOR YEARS WHERE 29TH OF FEB IS A THING
+	//SET DEFUALT OBJECT 
+	public Date (int dayPara, int monthPara, int yearPara) {
+		
+	}
+	
+	public Date (Date other) 
+	{
+		this._day = other._day;
+		this._month = other._month;
+		this._year = other._year;
+		
+	}
 
 }
